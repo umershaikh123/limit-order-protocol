24ce9adb60e05476d7ea69a3a131f549907e0a67
umershaikh123 <umershaikh217@gmail.com>
2025-08-01 07:42:32 +0500

    config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,23 +1,62 @@
-require('@matterlabs/hardhat-zksync-deploy');
-require('@matterlabs/hardhat-zksync-solc');
-require('@nomicfoundation/hardhat-chai-matchers');
-require('solidity-coverage');
-require('solidity-docgen');
-require('hardhat-dependency-compiler');
-require('hardhat-deploy');
-require('hardhat-gas-reporter');
-require('hardhat-tracer');
-require('dotenv').config();
-const { oneInchTemplates } = require('@1inch/solidity-utils/docgen');
-const { Networks, getNetwork } = require('@1inch/solidity-utils/hardhat-setup');
+require("@matterlabs/hardhat-zksync-deploy");
+require("@matterlabs/hardhat-zksync-solc");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("solidity-coverage");
+require("solidity-docgen");
+require("hardhat-dependency-compiler");
+require("hardhat-deploy");
+require("hardhat-gas-reporter");
+require("hardhat-tracer");
+require("dotenv").config();
+const { oneInchTemplates } = require("@1inch/solidity-utils/docgen");
+const { Networks, getNetwork } = require("@1inch/solidity-utils/hardhat-setup");
 
-if (getNetwork().indexOf('zksync') !== -1) {
-    require('@matterlabs/hardhat-zksync-verify');
+if (getNetwork().indexOf("zksync") !== -1) {
+    require("@matterlabs/hardhat-zksync-verify");
 } else {
-    require('@nomicfoundation/hardhat-verify');
+    require("@nomicfoundation/hardhat-verify");
 }
 
-const { networks, etherscan } = (new Networks()).registerAll();
+const { networks, etherscan } = new Networks().registerAll();
+
+const mainnetForkConfig = {
+    mainnetFork: {
+        url: process.env.MAINNET_RPC_URL,
+        forking: {
+            url: process.env.MAINNET_RPC_URL,
+
+            blockNumber: 18500000,
+        },
+        chainId: 1,
+        gas: 12000000,
+        gasPrice: 20000000000,
+        accounts: [
+            "f38837fb22ddf0b632e3a7f013c2f90ed588ae408a0119fb896e5e1f9e3c9b1d",
+        ],
+    },
+    // Alternative: local hardhat network with mainnet fork
+    localhost: {
+        url: "http://127.0.0.1:8545",
+        chainId: 31337,
+        forking: {
+            url: process.env.MAINNET_RPC_URL,
+            blockNumber: 18500000,
+        },
+    },
+};
+
+const mergedNetworks = {
+    ...networks,
+    ...mainnetForkConfig,
+    hardhat: {
+        forking: {
+            url: process.env.MAINNET_RPC_URLk,
+        },
+        chainId: 31337,
+        gas: 12000000,
+        gasPrice: 20000000000,
+    },
+};
 
 module.exports = {
     etherscan,
@@ -25,17 +64,17 @@ module.exports = {
         enableAllOpcodes: true,
     },
     solidity: {
-        version: '0.8.23',
+        version: "0.8.23",
         settings: {
             optimizer: {
                 enabled: true,
                 runs: 1_000_000,
             },
-            evmVersion: networks[getNetwork()]?.hardfork || 'shanghai',
+            evmVersion: networks[getNetwork()]?.hardfork || "shanghai",
             viaIR: true,
         },
     },
-    networks,
+    networks: mergedNetworks,
     namedAccounts: {
         deployer: {
             default: 0,
@@ -43,24 +82,24 @@ module.exports = {
     },
     gasReporter: {
         enable: true,
-        currency: 'USD',
+        currency: "USD",
     },
     dependencyCompiler: {
         paths: [
-            '@1inch/solidity-utils/contracts/mocks/TokenCustomDecimalsMock.sol',
-            '@1inch/solidity-utils/contracts/mocks/TokenMock.sol',
-            '@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol',
+            "@1inch/solidity-utils/contracts/mocks/TokenCustomDecimalsMock.sol",
+            "@1inch/solidity-utils/contracts/mocks/TokenMock.sol",
+            "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol",
         ],
     },
     zksolc: {
-        version: '1.4.0',
-        compilerSource: 'binary',
+        version: "1.4.0",
+        compilerSource: "binary",
         settings: {},
     },
     docgen: {
-        outputDir: 'docs',
+        outputDir: "docs",
         templates: oneInchTemplates(),
-        pages: 'files',
-        exclude: ['mocks'],
+        pages: "files",
+        exclude: ["mocks"],
     },
 };
